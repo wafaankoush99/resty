3acde900a840a90aa752bfced2cade91e2ab3de5
wafaankoush99 <wafadirawe@gmail.com>
2021-06-20 19:14:12 +0300

    resty app

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return(
+      <p>
+        &copy; All Rights Reserved 2021
+      </p>
+    )
+  }
+
+export default Footer;
