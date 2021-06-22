ce788cd1beaa3a18b05529ba45f9c1b6db7cef81
wafaankoush99 <wafadirawe@gmail.com>
2021-06-23 02:28:47 +0300

    updating

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,61 +1,6 @@
 import React from 'react';
 import './Form.scss';
 
-// class Form extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             userMethod: '',
-//             userURL: '',
-//         }
-//     }
-
-//     handleChanges = e => {
-//         this.state.userURL = e.target.value;
-//         // console.log(this.state.userURL)
-//         // let method = e.target.value;
-//         // this.setState({ userURL: URL })
-//     }
-//     handleClickBtns = e => {
-//         e.preventDefault();
-//         let userURL = this.state.userURL;
-//         this.setState({ userURL })
-//     }
-
-//     handleMethod = e => {
-//         e.preventDefault();
-//         let method = e.target.value;
-//         // let method = e.target.value;
-//         this.setState({ userMethod: method })
-//     }
-
-
-//     render() {
-//         return (
-//             <div className="formc">
-//                 <form>
-//                     <div className="fdiv">
-//                         URL:<input type="url" onChange={this.handleChanges}></input>
-//                         <button type="submit" onClick={this.handleClickBtns}>Go</button>
-//                     </div>
-
-//                     <br></br>
-//                     <div className="sdiv">
-//                         <button onClick={this.handleMethod} value="GET">GET</button>
-//                         <button onClick={this.handleMethod} value="POST">POST</button>
-//                         <button onClick={this.handleMethod} value="PUT">PUT</button>
-//                         <button onClick={this.handleMethod} value="DELETE">DELETE</button>
-//                     </div>
-
-//                 </form>
-//                 <section>
-//                     <div> {this.state.userMethod} {this.state.userURL}</div>
-//                 </section>
-//             </div>
-//         )
-//     }
-// }
-
 class Form extends React.Component {
     constructor(props) {
         super(props);
@@ -78,15 +23,32 @@ class Form extends React.Component {
         e.preventDefault();
         // this.state.userURL = e.target.value;
         let raw = await fetch(this.state.userURL);
+
+        let header= await fetch(this.state.userURL).then((response) => {    
+            for (let pair of response.headers.entries()) {
+                let length = pair.length
+                let string = '';
+                for (let i =0;i<length;i++){
+                    if (i %2 === 0){
+                        string = string + pair[i] + ': ' + pair[i+1]
+                    }
+                }
+                return string;
+            }
+        }); 
+
+
         // console.log('rrrr', raw);
         let data = await raw.json();
         // console.log('ddddd', data);
         const results = data.results;
         // this.state.results = results;
         const count = data.count;
+        // const header = data.header;
         // console.log('hhhhhhhhh', results);
-        this.props.handler(results, count);
-        this.setState(results)
+        console.log('hhhh', header);
+        this.props.handler(results, count, header);
+        // this.setState(results)
 
     }
 
@@ -122,8 +84,9 @@ class Form extends React.Component {
                         <button onClick={this.handleMethod} value="PUT">PUT</button>
                         <button onClick={this.handleMethod} value="DELETE">DELETE</button>
                     </div>
-
+                    <div> {this.state.userMethod}  :  {this.state.userURL} </div>
                 </form>
+                
             </div>
         )
     }
